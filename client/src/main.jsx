90f9d94b6aae4c76687de9df8dd567594914a0fe
SamandarTemirxodjayev <iwtuz166@gmail.com>
2023-04-20 16:11:37 +0500

    test

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,9 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+    // <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
+    </BrowserRouter>
+    // </React.StrictMode>
 )
