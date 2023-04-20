0edab74657fadb107ff4c60ea7259f0fecb03a9c
SamandarTemirxodjayev <iwtuz166@gmail.com>
2023-04-20 16:32:19 +0500

    default

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  ssr: true,
-  server:{
-    watch:{
-      usePolling:true,
-    }
-  }
 })
